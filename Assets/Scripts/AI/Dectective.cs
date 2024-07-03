2a097e49912e9c0dfb45815e2bd962799dbda003
Hoodrabbit <micyle1022@naver.com>
2024-07-03 18:35:41 +0900

    오류 다 무시하고 이렇게 돌아가지는지만 확인하려고 만든 거라서 오류가 엄청납니다

---
+++ Assets/Scripts/AI/Dectective.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Dectective : Ai, IPointerClickHandler
+{
+    string hello = "Hello World!";
+    public Text ActorText;
+    RpcManager rpcManager;
+
+    private void OnMouseDown()
+    {
+        // GameManager.Instance.voteManager.InitiateVote();
+        //PV.RPC("Invisibility", RpcTarget.All);
+    }
+
+    private void Start()
+    {
+        rpcManager = GetComponent<RpcManager>();
+        //ActorText.text = hello;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!PV.IsMine)
+        {
+            ActorText.text = hello;
+        }
+    }
+
+
+
+
+}
