92127e23900bc136ffafa68e2c4b89e411223af0
Hoodrabbit <micyle1022@naver.com>
2024-09-13 21:04:42 +0900

    캐릭터 크기 조정

--- Assets/DiceMissionObjectScript.cs
+++ Assets/DiceMissionObjectScript.cs
@@ -1,10 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class DiceMissionObjectScript : MonoBehaviour, IPointerClickHandler
 {
+    public Action IsDiceinCup;
+
+
+    public Sprite Normal_State_Cup;
+    public Sprite Open_State_Cup;
+
+    Image CupImg;
 
 
     public bool Dice = false;
@@ -13,7 +22,7 @@ public class DiceMissionObjectScript : MonoBehaviour, IPointerClickHandler
 
     public int NowPos;
 
-
+    RectTransform rect;
 
     //만약 클릭했을 때 해당 클릭한 오브젝트의 위치 또는 애니메이션을 실행시켜
 
@@ -21,17 +30,49 @@ public class DiceMissionObjectScript : MonoBehaviour, IPointerClickHandler
     //// Start is called before the first frame update
     void Start()
     {
-        if(transform.parent.CompareTag("Dice"))
+        CupImg = GetComponent<Image>();
+
+        rect = GetComponent<RectTransform>();
+
+        rect.anchoredPosition = new Vector2(rect.anchoredPosition.x, 100);
+
+        if (transform.parent.CompareTag("Dice"))
         {
             Dice = true;
         }
+
+        StartCoroutine(CloseCup());
     }
 
-    //// Update is called once per frame
-    //void Update()
-    //{
 
-    //}
+    IEnumerator CloseCup()
+    {
+
+        //컵을 닫는 연출 정확히는 0으로 이동시키는 연출
+         
+        float TTime = 0f;
+
+        while(TTime < 3f)
+        {
+            TTime += Time.deltaTime;
+            rect.anchoredPosition = Vector3.Lerp(rect.anchoredPosition, new Vector2(rect.anchoredPosition.x,0), TTime/3);
+            yield return null;
+        }
+        
+
+
+
+
+        
+    }
+
+
+
+    public void SelectReady()
+    {
+        ChooseDice = true;
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
 
@@ -42,9 +83,14 @@ public class DiceMissionObjectScript : MonoBehaviour, IPointerClickHandler
             if (Dice == true)
             {
                 //gameClear; 게임 종료 
+                IsDiceinCup?.Invoke();
+                //이벤트 제작
             }
             else
             {
+
+                Debug.Log("게임오버");
+                IsDiceinCup?.Invoke();
                 //gameInitialize; 게임 초기화
             }
         }
@@ -56,7 +102,7 @@ public class DiceMissionObjectScript : MonoBehaviour, IPointerClickHandler
     {
         //컵을 들어올렸을 때 주사위가 존재하냐 안하냐에 따라서 재시작 혹은 미션 클리어로 만들어줘야 할 것 같음
 
-
+        StartCoroutine(OpenCup());
 
     }
 
@@ -65,7 +111,49 @@ public class DiceMissionObjectScript : MonoBehaviour, IPointerClickHandler
         NowPos = num;
     }
 
+    IEnumerator OpenCup()
+    {
+        //yield return new WaitForSeconds(1);
+        
+        //컵 이미지 변경 
+        //컵의 위치를 변경
+        float TTime = 0f;
 
+        CupImg.sprite = Open_State_Cup;
+
+
+        while (TTime < 2f)
+        {
+            TTime += Time.deltaTime;
+            rect.anchoredPosition = Vector3.Lerp(rect.anchoredPosition, new Vector2(rect.anchoredPosition.x, 100), TTime / 2);
+            yield return null;
+        }
+
+
+
+
+
+    }
+
+    void Initalizing()
+    {
+        //스프라이트 원본 스프라이트로 변경
+        //위치 조정
+
+        ChooseDice = false;
+
+
+    }
+
+    public void DiceIn()
+    {
+        Debug.Log("있어요");
+    }
+
+    public void DiceNotIn()
+    {
+        Debug.Log("없어요");
+    }
 
 
 }
--- Assets/workSpace/Dong/Scripts/Mission/ChooseDIceMission/ChooseDiceMission.cs
+++ Assets/workSpace/Dong/Scripts/Mission/ChooseDIceMission/ChooseDiceMission.cs
@@ -1,12 +1,17 @@
-using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChooseDiceMission : MonoBehaviour
+public class ChooseDiceMission : Quest
 {
+    //97번줄 이벤트 수정해야함중요
+
+
     public GameObject Cup;
     public GameObject CupInDice;
+
+    public Action ClearQuestevt;
 
 
     public float StartTime = 0f;
@@ -16,32 +21,28 @@ public class ChooseDiceMission : MonoBehaviour
     bool AlreadyMake = false;
     int count = 0;
 
+    bool ChooseActivate = false;
 
-    public List<RectTransform> CupRectLIst  = new List<RectTransform>();
+
+    public List<RectTransform> CupRectList  = new List<RectTransform>();
+    public List<DiceMissionObjectScript> CupObjList = new List<DiceMissionObjectScript>();
     public List<Vector2>CupPos = new List<Vector2>();
 
 
 
-    int Num = 3;
-    int mixcount = 5;
+
+    public Vector2 startpos;
+    public Vector2 endpos;
 
 
     public int StartNum;
     public List<int> StartNumList= new List<int>();
-    
-    List<int> StartNum_Initalize = new List<int> { 0, 1, 2 };
-
-
-
 
     public int EndNum;
     public List<int> EndNumList = new List<int>();
-    List<int> EndNumList_Initalize = new List<int> { 0, 1, 2 };
 
 
-
-
-    private void Start()
+    protected override void Start()
     {
 
         for (int i = -1; i < 2; i++)
@@ -50,26 +51,101 @@ public class ChooseDiceMission : MonoBehaviour
             CupPos.Add(new Vector2(j * i, 0));
         }
 
-        //StartNumList= StartNum_Initalize;
-        //EndNumList = EndNumList_Initalize;
-        MakeGame();
+        if (CupRectList.Count == 0)
+        {
+            while (count < 3)
+            {
+                int randomNum = UnityEngine.Random.Range(0, 2);
+
+                if (randomNum == 0)
+                {
+                    if (Check < 2)
+                    {
+                        InstantiateJustCup();
+                    }
+
+
+                }
+                else
+                {
+
+                    if (!AlreadyMake)
+                    {
+                        InstantiateCupinDice();
+                    }
+
+                }
+
+            }
+        }
+
+        StartCoroutine(MoveCup_2(5));
+
+
+
+       
+
+       
     }
+
+    private void Update()
+    {
+        if (ChooseActivate) //이벤트로 전환예정
+        {
+            Debug.Log(CupObjList.Count);
+
+            foreach (var cupp in CupObjList) //현재 미션 초기 구성에서 가장 마지막에 이뤄져야함
+            {
+                cupp.SelectReady();
+                cupp.IsDiceinCup += ClearQuestevt; //수정해야함 
+            }
+            ChooseActivate = false;
+        }
+    }
+
+
+
+    void ChangeChange(out int randNum,out int randNum_2)
+    {
+        randNum = UnityEngine.Random.Range(0, 3);
+
+        startpos = CupRectList[randNum].anchoredPosition;
+
+
+        randNum_2 = UnityEngine.Random.Range(0, 3);
+
+        while (randNum_2 == randNum)
+        {
+            randNum_2 = UnityEngine.Random.Range(0, 3);
+        }
+        endpos = CupRectList[randNum_2].anchoredPosition;
+
+
+        //Debug.Log(startpos);
+        //Debug.Log(endpos);
+
+        //Debug.Log(randNum + " , " + randNum_2);
+       // StartCoroutine(MoveCup_2(randNum, randNum_2));
+
+    }
+
+
 
     void RandomizeCup()
     {
         int AddCount = 0;
         while(AddCount < 5)
         {
-            int RandomNum = Random.Range(0, 3);
+            int RandomNum = UnityEngine.Random.Range(0, 3);
             StartNum = RandomNum;
             StartNumList.Add(RandomNum);
 
 
-            RandomNum = Random.Range(0, 3);
+            RandomNum = UnityEngine.Random.Range(0, 3);
             EndNum = RandomNum;
             while (StartNum == EndNum)
             {
-                RandomNum = Random.Range(0, 3);
+                RandomNum = UnityEngine.Random.Range(0, 3);
                 EndNum = RandomNum;
             }
             EndNumList.Add(RandomNum);
@@ -92,22 +168,22 @@ public class ChooseDiceMission : MonoBehaviour
 
             Debug.Log(StartNumList[count] + "   ,   " + EndNumList[count]);
 
-            Debug.Log(CupRectLIst[StartNumList[count]].gameObject.name);
-            Debug.Log(CupRectLIst[EndNumList[count]].gameObject.name);
+            Debug.Log(CupRectList[StartNumList[count]].gameObject.name);
+            Debug.Log(CupRectList[EndNumList[count]].gameObject.name);
             while (StartTime <= 4f)
             {
                 //Debug.Log(StartTime);
                 StartTime += Time.deltaTime;
 
-                //CupPos[StartNum] = CupRectLIst[StartNum].anchoredPosition;
-                //CupPos[EndNum] = CupRectLIst[EndNum].anchoredPosition;
+                //CupPos[StartNum] = CupRectList[StartNum].anchoredPosition;
+                //CupPos[EndNum] = CupRectList[EndNum].anchoredPosition;
 
 
                
-                CupRectLIst[StartNumList[count]].anchoredPosition = Vector3.Lerp(CupRectLIst[StartNumList[count]].anchoredPosition, CupPos[EndNumList[count]], StartTime / 4f);
+                CupRectList[StartNumList[count]].anchoredPosition = Vector3.Lerp(CupRectList[StartNumList[count]].anchoredPosition, CupPos[EndNumList[count]], StartTime / 4f);
                 
                 
-                CupRectLIst[EndNumList[count]].anchoredPosition = Vector3.Lerp(CupRectLIst[EndNumList[count]].anchoredPosition, CupPos[StartNumList[count]], StartTime / 4f);
+                CupRectList[EndNumList[count]].anchoredPosition = Vector3.Lerp(CupRectList[EndNumList[count]].anchoredPosition, CupPos[StartNumList[count]], StartTime / 4f);
                 
                 yield return null;
             }
@@ -122,7 +198,48 @@ public class ChooseDiceMission : MonoBehaviour
 
     }
 
+    IEnumerator MoveCup_2(int trycount)
+    {
 
+        yield return new WaitForSeconds(0.5f);
+
+        if(trycount >0)
+        {
+            int rand_start, rand_end;
+
+            ChangeChange(out rand_start, out rand_end);
+            Debug.Log(rand_start + " , " + rand_end);
+
+            StartTime = 0f;
+            while (StartTime <= 0.2f)
+            {
+                //Debug.Log(StartTime);
+                StartTime += Time.deltaTime;
+
+                //CupPos[StartNum] = CupRectList[StartNum].anchoredPosition;
+                //CupPos[EndNum] = CupRectList[EndNum].anchoredPosition;
+
+
+
+                CupRectList[rand_start].anchoredPosition = Vector3.Lerp(CupRectList[rand_start].anchoredPosition, endpos, StartTime / 1f);
+
+
+                CupRectList[rand_end].anchoredPosition = Vector3.Lerp(CupRectList[rand_end].anchoredPosition, startpos, StartTime / 1f);
+
+                yield return null;
+            }
+            //StartTime = 0f;
+            //yield return new WaitForSeconds(0.5f);
+            StartCoroutine(MoveCup_2(trycount - 1));
+        }
+        else
+        {
+            ChooseActivate = true;
+            yield return null;
+        }
+        
+
+    }
 
     void MakeGame()
     {
@@ -130,11 +247,11 @@ public class ChooseDiceMission : MonoBehaviour
 
 
 
-        if(CupRectLIst .Count == 0)
+        if(CupRectList.Count == 0)
         {
             while (count < 3)
             {
-                int randomNum = Random.Range(0, 2);
+                int randomNum = UnityEngine.Random.Range(0, 2);
 
                 if (randomNum == 0)
                 {
@@ -185,8 +302,8 @@ public class ChooseDiceMission : MonoBehaviour
         cupTransform.anchoredPosition = CupPos[count];
         Check++;
         count++;
-        CupRectLIst.Add(cupTransform);
-
+        CupRectList.Add(cupTransform);
+        CupObjList.Add(cupTransform.gameObject.GetComponent<DiceMissionObjectScript>());
         //CupPos.Add(cupTransform);
     }
 
@@ -196,7 +313,8 @@ public class ChooseDiceMission : MonoBehaviour
         cupTransform.anchoredPosition = CupPos[count];
         AlreadyMake = true;
         count++;
-        CupRectLIst.Add(cupTransform);
+        CupRectList.Add(cupTransform);
+        CupObjList.Add(cupTransform.transform.GetComponentInChildren<DiceMissionObjectScript>());
         //CupPos.Add(cupTransform);
     }
 
@@ -204,11 +322,87 @@ public class ChooseDiceMission : MonoBehaviour
 
 
 
+    void ChangeCupPos()
+    {
+        //GameObject[] cups;
+
+        //if (cups.Length == 0) return;
+
+        //// 현재 컵들의 위치를 저장
+        //Vector3[] initialPositions = new Vector3[cups.Length];
+        //for (int i = 0; i < cups.Length; i++)
+        //{
+        //    initialPositions[i] = cups[i].transform.position;
+        //}
+
+        //// 컵들의 위치를 서로 교환 (여기서는 첫 번째 컵이 마지막 컵으로, 나머지 컵은 한 칸씩 앞으로 이동)
+        //for (int i = 0; i < cups.Length; i++)
+        //{
+        //    int nextIndex = (i + 1) % cups.Length; // 마지막 컵이 첫 번째로 이동하게끔 설정
+        //    cups[i].transform.position = initialPositions[nextIndex];
+        //}
 
 
+    }
+
+    protected override void ClearQuest()
+    {
+        //CupObjList.
+        base.ClearQuest();
+        QuestManager.Instance.OpenClearPanel();
+    }
 
 
+    public override void InitalizeQuest()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override int GetQuestID()
+    {
+        throw new System.NotImplementedException();
+    }
 }
+
+
+
+
+
+
+//MoveCup_2();
+
+//int randNum = Random.Range(0, 3);
+//startpos = CupRectList[randNum].anchoredPosition;
+
+
+//int randNum_2 = Random.Range(0, 3);
+
+//while(randNum_2 == randNum)
+//{
+//    randNum_2 = Random.Range(0, 3);
+//}
+//endpos = CupRectList[randNum_2].anchoredPosition;
+
+
+//Debug.Log(startpos);
+//Debug.Log(endpos);
+
+//Debug.Log(randNum + " , " + randNum_2);
+//StartCoroutine(MoveCup_2(randNum, randNum_2));
+
+
+
+//StartNumList= StartNum_Initalize;
+//EndNumList = EndNumList_Initalize;
+
+///주석 해제할 거
+//MakeGame();
+
+
+
+
+
+
 
 
 /*
